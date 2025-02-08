a0289179ea4dfbada07acc41b95730a90617d59b
Fahlevi20 <rozakalfi@gmail.com>
2025-02-08 11:59:54 +0700

    change to async function

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const { HumanMessage, SystemMessage } = require("@langchain/core/messages");
 const model = new ChatOllama({model:'qwen2.5-coder:3b'})
 
 
-const messages = [
-    new SystemMessage("you are an assistant"),
-    new HumanMessage("halo!"),
-];
 
-model.invoke(messages);
+async function main() {
+
+    try{
+        const messages = [
+            new SystemMessage("you are an assistant"),
+            new HumanMessage("halo!"),
+        ];
+        const result = await model.invoke(messages);
+        console.log(result);
+    } catch(error){
+        console.error("error:",error)
+    }
+    
+}
+
+main();
