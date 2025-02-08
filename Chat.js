bd4b1ff0f1b03115998bed4279b8c6e51686d7a9
Fahlevi20 <rozakalfi@gmail.com>
2025-02-08 11:47:09 +0700

    add system message to index.js

--- Chat.js
+++ Chat.js
@@ -1,7 +1,6 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
-
 const messages = [
     new SystemMessage("you are an assistant"),
     new HumanMessage("halo!"),
-]
+];
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { ChatOllama, Ollama } from "@langchain/ollama";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
 
 const model = new ChatOllama({model:'qwen2.5-coder:3b'})
 
+
+const messages = [
+    new SystemMessage("you are an assistant"),
+    new HumanMessage("halo!"),
+];
