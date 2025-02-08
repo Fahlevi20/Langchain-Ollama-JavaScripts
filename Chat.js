5fc81e2d3219d6cf247729f589208bc04cee5a71
Fahlevi20 <rozakalfi@gmail.com>
2025-02-08 11:45:10 +0700

    add messages in chat.js

---
+++ Chat.js
@@ -0,0 +1,6 @@
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
+
+const messages = [
+    new SystemMessage("you are an assistant"),
+    new HumanMessage("halo!")
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import { ChatOllama, Ollama } from "@langchain/ollama";
 
 const model = new ChatOllama({model:'qwen2.5-coder:3b'})
+
