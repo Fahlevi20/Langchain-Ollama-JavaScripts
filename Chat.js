9635cb308f515bca455287269d43b16503c86f0c
Fahlevi20 <rozakalfi@gmail.com>
2025-02-08 11:45:27 +0700

    add changes

--- Chat.js
+++ Chat.js
@@ -3,4 +3,5 @@ import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
 const messages = [
     new SystemMessage("you are an assistant"),
-    new HumanMessage("halo!")
+    new HumanMessage("halo!"),
+]
